195a1275db3e2e8e3ee256c9f14355d945e0704f
alexRhine22 <alextherhine@gmail.com>
2020-07-18 13:13:03 -0700

    Added header nav

--- assets/nav.js
+++ assets/nav.js
@@ -1,5 +1,20 @@
 let mobileNavButton = document.getElementById("mobile-button");
+let asideNav = document.getElementById("aside-nav");
+let header = document.getElementById("js-header");
 
 mobileNavButton.addEventListener("click", (e) => {
   mobileNavButton.classList.toggle("active");
+  asideNav.classList.toggle("active");
+});
+
+let scrollPosY = window.scrollY;
+window.addEventListener("scroll", (e) => {
+  scrollPosY = window.scrollY;
+  if (scrollPosY > 0) {
+    header.classList.add("scrolled");
+    asideNav.classList.remove("unscrolled");
+  } else {
+    header.classList.remove("scrolled");
+    asideNav.classList.add("unscrolled");
+  }
 });
